83cf9b77d0afb15f2c22aaeb6eddbc106bf658ae
holdenhughes <holdenmh121489@gmail.com>
2015-11-24 11:02:13 -0500

    Initial commit

---
+++ src/to_do_clojure/core.clj
@@ -0,0 +1,20 @@
+(ns to-do-clojure.core
+  (:gen-class))
+
+(def to-dos (atom []))
+
+(defn add-to-do [input]
+  (reset! to-dos (conj (deref to-dos) input))
+  )
+
+(defn -main [& args]
+  (println "type to-do and hit enter")
+
+  (loop []
+    (let [input (read-line)]
+      (when (not= input "q")
+        (add-to-do input)
+        (recur))))
+
+  (println (deref to-dos))
+  )
